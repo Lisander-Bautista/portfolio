2460d85710a3fdd59ae560d645bd05d053e38e7d
Lisander-Bautista <lixanderinfante@gmail.com>
2026-02-15 11:18:29 -0400

    Primera actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from "./components/content/content.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ContentComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor(private router: Router) {}
+
+ngAfterViewInit() {
+    this.router.events.subscribe(event => {
+      if (window.HSStaticMethods) {
+        setTimeout(() => {
+          window.HSStaticMethods.autoInit();
+        }, 100);
+      }
+    });
+  }
 }
